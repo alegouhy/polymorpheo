152d3efb685a33bfe23e9df622bdf08570efd2c9
ClementCaporal <caporal.clement@gmail.com>
2026-05-20 17:40:16 +0200

    Add core functionality for contour bridging and registration, update .gitignore to exclude reports

--- contours2mesh/__init__.py
+++ contours2mesh/__init__.py
@@ -1,10 +1,15 @@
 """Public API for contours2mesh."""
 
 from . import energy, register, transfo, utils
+from .io import io
+from .core import bridge_contours, register_slices
 
 __all__ = [
     "energy",
     "register",
     "transfo",
     "utils",
+    "io",
+    "bridge_contours",
+    "register_slices",
 ]
---
+++ scripts/npz_registration.py
@@ -0,0 +1,92 @@
+import os
+os.environ['JAX_PLATFORMS'] = 'cpu'
+os.environ["JAX_PLATFORM_NAME"] = "cpu"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import contours2mesh.utils as utils
+import contours2mesh.energy as energy
+import contours2mesh as c2m
+from pathlib import Path
+from importlib.resources import files
+
+
+REPO_ROOT = Path(__file__).resolve().parents[1]
+REPORTS_DIR = REPO_ROOT / "reports"
+
+datadir_path = str(files("contours2mesh.data").joinpath("sample_contours.npz"))
+
+io = c2m.io(
+    datadir=datadir_path.replace("/sample_contours.npz", ""),
+    names=["sample_contours"],
+    npts=10,
+    npts_min=5,
+    normalise=True
+    )
+
+
+polylines_raw = io.load()
+
+
+# spacing = np.array([0.1, 0.1, 1.25])
+spacing = np.array([1, 1, 12.5])
+npts = 100
+npts_min = 5
+icp_niter = 20
+bidir = True
+thr_conn = [0.2, 0.5]
+niter = 5
+
+method = 4              # serial registration method 
+
+mesher = c2m.bridge_contours(thr_conn=thr_conn, sealed=True)
+
+
+  
+transfo = 'rigid'
+print('method ' + str(method) + ', ' + transfo)
+init = 'centroid'
+reg = c2m.register_slices(method, transfo, init, bidir=bidir,
+                          plot=False, xlim=io.xlim, ylim=io.ylim)
+polylines_rig = reg.compute(polylines_raw)
+meshes_rig = mesher.compute(polylines_rig)
+io.save(meshes_rig, REPORTS_DIR, suffix='rig_met-'+str(method))
+
+
+transfo = 'affine'
+print('method ' + str(method) + ', ' + transfo)
+init = 'identity'
+reg = c2m.register_slices(method, transfo, init, bidir=bidir,
+                          plot=False, xlim=io.xlim, ylim=io.ylim)
+polylines_aff = reg.compute(polylines_rig)
+meshes_aff = mesher.compute(polylines_aff)
+io.save(meshes_aff, REPORTS_DIR, suffix='aff_met-'+str(method))
+
+
+
+# transfo = 'polynomial'
+# degree = 2
+# init = 'identity'
+# print('method ' + str(method) + ', ' + transfo)
+# reg = c2m.register_slices(method, transfo, init, degree=degree, bidir=bidir)
+# polylines_quad = reg.compute(polylines_aff)
+# meshes_quad = mesher.compute(polylines_quad)
+# io.save(meshes_quad, outdir, suffix='quad_met-'+str(method))
+
+
+transfo = 'deformable'
+print('method ' + str(method) + ', ' + transfo)
+lr = 1e-2
+wreg = 5e-1
+int_steps = 16
+sigma = 1e-1
+icp_niter = 50
+fit_fun = energy.pointdist(agg='mean', bidir=bidir)
+# regul_fun = energy.alap(transfo='similarity', normtype='l2')
+regul_fun = energy.grad_disp(l=2)
+reg = c2m.register_slices(method, transfo, fit_fun=fit_fun, regul_fun=regul_fun, niter=niter,
+                          icp_niter=icp_niter, lr=lr, wreg=wreg, sigma=sigma, int_steps=int_steps)
+polylines_defo = reg.compute(polylines_aff)
+meshes_defo = mesher.compute(polylines_defo)
+io.save(meshes_defo, REPORTS_DIR, suffix='defo_met-'+str(method)+'-'+str(niter))
