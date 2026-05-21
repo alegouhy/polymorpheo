rework reg_deformable: rename kernel_disp to polytransfo, fix normalization and chain application

polymorpheo/register.py
@@ -8,8 +8,8 @@ import polymorpheo.energy as energy
 import polymorpheo.transfo as transfo_ops
 import polymorpheo.utils as utils
 
-# %%
 
+# %%
 
 def load_meshes(ref_mesh, mov_mesh, normalise=True):
     ref_meshes = [ref_mesh] if hasattr(ref_mesh[0], "shape") else ref_mesh
@@ -281,7 +281,7 @@ class reg_deformable:
         self.sigma = sigma
         self.int_steps = int_steps
         self.normalise = normalise
-        self.kernel_fun = transfo_ops.kernel_disp(
+        self.polytransfo = transfo_ops.polytransfo(
             sigma=sigma, int_steps=int_steps, rk=rk
         )
         self.fit_fun = fit_fun
@@ -308,7 +308,7 @@ class reg_deformable:
         )
 
         opti_step = self.make_opti_step(
-            fit_fun, regul_fun, wreg, self.kernel_fun, self.optimizer
+            fit_fun, regul_fun, wreg, self.polytransfo, self.optimizer
         )
         if not self.plot:
             self.opti_loop = self.make_opti_loop(opti_step, tol, niter, warmup_steps)
@@ -318,31 +318,34 @@ class reg_deformable:
             )
 
     def compute(self, ref_mesh, mov_mesh):
-        ref_mesh_list, mov_mesh, mu, amp = load_meshes(
-            ref_mesh, mov_mesh, self.normalise
-        )
-        mov_pts, mov_simps, _, mov_labs = mov_mesh
+        
+        ref_mesh_list, mov_mesh_n, mu, amp = load_meshes(ref_mesh, mov_mesh, self.normalise)
+        mov_pts_n, mov_simps, _, mov_labs = mov_mesh_n
 
         if self.cpts_ratio == 1:
-            cpts = mov_pts
+            cpts = mov_pts_n
         else:
-            ncpts = int(mov_pts.shape[0] * self.cpts_ratio)
-            _, cpts = utils.farthest_point_sampling(mov_pts, ncpts)
+            ncpts = int(mov_pts_n.shape[0] * self.cpts_ratio)
+            _, cpts = utils.farthest_point_sampling(mov_pts_n, ncpts)
+
         theta0 = jnp.zeros_like(cpts)
         opt_state = self.optimizer.init(theta0)
+        theta, losses = self.opti_loop(theta0, opt_state, cpts, mov_mesh_n, ref_mesh_list)
 
-        theta, losses = self.opti_loop(theta0, opt_state, cpts, mov_mesh, ref_mesh_list)
+        disp = self.polytransfo.compute(mov_pts_n, cpts, theta_lin=None, theta_trans=theta)
+        moved_mesh = utils.denormalise_meshes(
+            [(mov_pts_n + disp, mov_simps, None, mov_labs)], mu, amp
+        )[0]
 
-        disp = self.kernel_fun.compute(mov_pts, cpts, theta_lin=None, theta_trans=theta)
-        moved_pts = mov_pts + disp
-        moved_mesh = (moved_pts, mov_simps, None, mov_labs)
+        self.polytransfo.sigma = self.sigma * amp
+        self.polytransfo.set_params(cpts * amp + mu, theta_trans=theta * amp)
 
-        disp = disp * amp
-        moved_mesh = utils.denormalise_meshes([moved_mesh], mu, amp)[0]
+        return theta * amp, moved_mesh, losses
 
-        return theta, moved_mesh, losses
 
-    def make_opti_step(self, fit_fun, regul_fun, wreg, kernel_fun, optimizer):
+
+    def make_opti_step(self, fit_fun, regul_fun, wreg, polytransfo, optimizer):
+        
         @jax.jit
         def opti_step(theta, opt_state, cpts, mov_mesh, ref_mesh_list):
             loss, grads = jax.value_and_grad(energy.energy_total_fn)(
@@ -353,7 +356,7 @@ class reg_deformable:
                 fit_fun,
                 regul_fun,
                 wreg,
-                kernel_fun,
+                polytransfo,
             )
             updates, opt_state = optimizer.update(grads, opt_state, theta)
             theta = optax.apply_updates(theta, updates)
@@ -413,7 +416,7 @@ class reg_deformable:
                         break
 
                 if k % self.plot == 0:
-                    disp = self.kernel_fun.compute(
+                    disp = self.polytransfo.compute(
                         mov_pts, cpts, theta_lin=None, theta_trans=theta
                     )
                     moved_mesh_plot = (mov_pts + disp, mov_simps, None, mov_labs)

scripts/toy_registration.py
@@ -60,7 +60,6 @@ reg_rig = register.reg_linear(niter=niter, transfo=transfo, init=init, se=True, 
 
 t = time.time()
 rig, moved_contour_rig = reg_rig.compute(ref_contour, mov_contour)
-
 print(f"rigid done in: {time.time()-t:.2f} s")
 
 plt.subplot(2,3,2)
@@ -77,7 +76,7 @@ transfo = 'similarity' # 'affine'
 reg_aff = register.reg_linear(niter=niter, transfo=transfo, se=True, plot=False, bidir=bidir)
 
 t = time.time()
-aff, moved_contour_aff = reg_aff.compute(ref_contour, moved_contour_rig, T0=rig)
+aff, moved_contour_aff = reg_aff.compute(ref_contour, moved_contour_rig)
 print(f"affine done in: {time.time()-t:.2f} s")
 
 plt.subplot(2,3,3)
@@ -124,7 +123,7 @@ plt.title('cubic')
 #%% non-rigid ICP
 
 niter = 50
-lr = [1e-2, 1e-3, 1e-3]
+lr = [1e-2] # , 1e-3, 1e-3]
 wreg = [1e-1, 1e-2, 1e-2]
 int_steps = 16
 sigma = [1e-1, 1e-2, 1e-3]
@@ -135,15 +134,16 @@ regul_fun = energy.grad_disp(l_norm=2)
 moved_contour_defo = moved_contour_aff
 
 losses = []
+reg_defos = []
 t = time.time()
 for i in range(len(lr)):
-    
+
     reg_defo = register.reg_deformable(niter=niter, fit_fun=fit_fun, regul_fun=regul_fun,
                                        lr=lr[i], wreg=wreg[i], sigma=sigma[i], int_steps=int_steps,
-                                       tol=tol, plot=False)
-    
+                                       tol=tol, plot=False, normalise=True)
+
     disp, moved_contour_defo, loss = reg_defo.compute(ref_contour, moved_contour_defo)
-    
+    reg_defos.append(reg_defo)
     losses.append(loss)
 
 print(f"deformable done in: {time.time()-t:.2f} s")
@@ -162,3 +162,34 @@ for i in range(len(lr)):
     plt.plot(losses[i])
     plt.suptitle('energy')
 plt.show()
+
+
+#%% test transfo chain compo
+
+import polymorpheo.transfo as transfo_ops
+
+rig_lin, rig_trans = utils.aff_dehmgn(rig)
+rig_transfo = transfo_ops.affine()
+rig_transfo.set_params(rig_lin, rig_trans)
+
+aff_lin, aff_trans = utils.aff_dehmgn(aff)
+aff_transfo = transfo_ops.affine()
+aff_transfo.set_params(aff_lin, aff_trans)
+
+
+chain = [rig_transfo, aff_transfo] + [rd.polytransfo for rd in reg_defos]
+moved_pts_chain = transfo_ops.apply_transfo_chain(chain, mov_pts)
+
+import jax.numpy as jnp
+print("max diff rig:", jnp.max(jnp.abs(moved_pts_chain - jnp.array(moved_contour_rig[0]))))
+
+plt.figure()
+# utils.plot_contour(ref_contour, col=[1,0,0])
+utils.plot_contour((moved_pts_chain, mov_simps, None, mov_labs), col=[0,0,1])
+utils.plot_contour(moved_contour_defo, col=[0,1,0])
+plt.title('chain (blue) vs sequential (green)')
+plt.show()
+
+
+
+